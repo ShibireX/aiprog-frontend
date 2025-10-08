a227c613137a06f79937245fac339a6f0b4a93c7
Rasmus Hugosson <rasmus.hugosson01@gmail.com>
2025-10-08 02:48:01 +0200

    Add basic footer

---
+++ components/ui/link-small.tsx
@@ -0,0 +1,21 @@
+import { cn } from '@/lib/utils'
+
+interface LinkSmallProps {
+  content: string
+  href: string
+  className?: string
+}
+
+export function LinkSmall({ content, href, className }: LinkSmallProps) {
+  return (
+    <a
+      href={href}
+      className={cn(
+        'font-semibold leading-relaxed text-gray-700 underline hover:text-blue-700',
+        className
+      )}
+    >
+      {content}
+    </a>
+  )
+}
